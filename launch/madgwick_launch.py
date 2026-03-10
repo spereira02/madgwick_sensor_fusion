a3c9681e1858fadad109043c548ea57872e1fb0d
spereira02 <spereira@ethz.ch>
2026-03-10 16:23:29 +0100

    Increased verbose on launch

--- launch/madgwick_launch.py
+++ launch/madgwick_launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
             executable='micro_ros_agent',
             name='micro_ros_agent',
             output='screen',
-            arguments=['serial', '--dev', '/dev/ttyUSB0']
+            arguments=['serial', '--dev', '/dev/ttyUSB0', 'v6']
         ),
         Node(
             package='rviz2',
